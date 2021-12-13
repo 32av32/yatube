d80b5c33ba6bf091323fc24b7e1d167c08b5698e
Isa Amerkhanov <isa.amerkhanov@gmail.com>
2021-12-13 17:44:10 +0300

    Added \'new_post\' view, template in posts app

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group
+from .forms import NewPostForm
 
 def index(request):
     # posts = Post.objects.order_by('-pub_date')[:10]
@@ -12,3 +13,14 @@ def group_posts(request, slug):
     # posts = Post.objects.filter(group=group).order_by('-pub_date')[:12]
     posts = list(Post.objects.filter(group=group).select_related('author').order_by('-pub_date')[:12])
     return render(request, 'group.html', {'group': group, 'posts': posts})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+            return redirect('index')
+        return render(request, 'new.html', {'form': form})
+    form = NewPostForm()
+    return render(request, 'new.html', {'form': form})
